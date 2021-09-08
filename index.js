e8351e80d3e30c7590bff27f7f61ccb5235f1ed5
amhanisa <azka@amhanisa.com>
2021-09-08 15:30:34 +0700

    v1

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const pool = mysql.createPool({
 });
 
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -21,79 +22,88 @@ app.get("/", (req, res) => {
 });
 
 app.get("/getAll", (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(`SELECT * FROM score`, (err, rows, fields) => {
+  const type = req.query.type;
+
+  pool.query(
+    `SELECT * FROM score WHERE type='${type}'`,
+    (err, rows, fields) => {
       console.log(rows);
       res.send(rows);
-      connection.release();
-    });
-  });
+    }
+  );
 });
 
 app.get("/getRanks", (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(
+  type = req.query.type;
+
+  if (type === "") {
+    pool.query(
       `SELECT * FROM score ORDER BY score DESC`,
       (err, rows, fields) => {
         console.log(rows);
         const ranks = rows;
 
-        // res.send(ranks);
-        connection.query(
+        pool.query(
           `SELECT MAX(last_update) as time FROM score`,
           (err, rows, fields) => {
             console.log(rows);
             const updateTime = rows[0].time;
-            connection.release();
             res.send({ ranks, updateTime });
           }
         );
       }
     );
-  });
+  } else {
+    pool.query(
+      `SELECT * FROM score WHERE type='${type}' ORDER BY score DESC`,
+      (ex, rows) => {
+        const ranks = rows;
+
+        pool.query(
+          `SELECT MAX(last_update) as time FROM score WHERE type='${type}'`,
+          (ex, rows) => {
+            let updateTime;
+            if (rows) {
+              updateTime = rows[0].time;
+            }
+
+            res.send({ ranks, updateTime });
+          }
+        );
+      }
+    );
+  }
 });
 
 app.post("/addTeam", (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(
-      `INSERT INTO score (team) values ('${req.body.team}')`,
-      (err, rows, fields) => {
-        if (err) throw err;
-        const id = rows.insertId;
-        res.status(200).send({ id: id });
-        connection.release();
-      }
-    );
-  });
+  pool.query(
+    `INSERT INTO score (team, type) values ('${req.body.team}', '${req.body.type}')`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      const id = rows.insertId;
+      res.status(200).send({ id: id });
+    }
+  );
 });
 
 app.post("/updateTeam", (req, res) => {
   const { id, team, score } = req.body;
 
-  pool.getConnection((err, connection) => {
-    connection.query(
-      `UPDATE score SET team='${team}', score=${score}, last_update=now() WHERE id=${id}`,
-      (err, rows, fields) => {
-        if (err) throw err;
-        res.status(200).send(rows);
-        connection.release();
-      }
-    );
-  });
+  pool.query(
+    `UPDATE score SET team='${team}', score=${score}, last_update=now() WHERE id=${id}`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.status(200).send(rows);
+    }
+  );
 });
 
 app.post("/deleteTeam", (req, res) => {
   const { id } = req.body;
 
-  pool.getConnection((err, connection) => {
-    connection.query(
-      `DELETE FROM score WHERE id=${id}`,
-      (err, rows, fields) => {
-        if (err) throw err;
-        res.status(200).send(rows);
-        connection.release();
-      }
-    );
+  pool.query(`DELETE FROM score WHERE id=${id}`, (err, rows, fields) => {
+    if (err) throw err;
+    res.status(200).send(rows);
   });
 });
 
---
+++ rally.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2021 at 07:47 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rally`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score`
+--
+
+CREATE TABLE `score` (
+  `id` int(11) NOT NULL,
+  `team` varchar(255) DEFAULT NULL,
+  `score` int(11) DEFAULT 0,
+  `type` enum('MANAGER','CONSUMENT','MEDIA','COMMUNITY') NOT NULL,
+  `last_update` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`id`, `team`, `score`, `type`, `last_update`) VALUES
+(57, '234', 0, 'MANAGER', '2021-09-08 04:16:22'),
+(58, '123', 1000, 'MANAGER', '2021-09-08 04:17:29'),
+(59, 'comunitas', 1000, 'COMMUNITY', '2021-09-08 05:42:42'),
+(60, 'comunirt', 200, 'COMMUNITY', '2021-09-08 05:42:36'),
+(61, 'konsum', 0, 'CONSUMENT', '2021-09-08 04:57:49'),
+(62, 'makan', 0, 'CONSUMENT', '2021-09-08 04:57:52'),
+(63, 'media', 0, 'MEDIA', '2021-09-08 04:57:57'),
+(64, 'meddd', 0, 'MEDIA', '2021-09-08 04:58:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
